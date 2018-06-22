8402938ffdd9d84f47d88bcd50804cac2b60da58
Darrell Hart <dahart@adobe.com>
2018-06-22 15:10:02 -0700

    added create donation page

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import os
 import base64
+import random
 
 from flask import Flask, render_template, request, redirect, url_for, session
 
-from model import Donation 
+from model import Donation, Donor
 
 app = Flask(__name__)
 
@@ -15,7 +16,16 @@ def home():
 def all():
     donations = Donation.select()
     return render_template('donations.jinja2', donations=donations)
-    
+
+@app.route('/create', methods=['GET', 'POST'])
+def create():
+    if request.method == 'POST':
+        donation = Donation(donor=request.form['name'], value=int(request.form['donation']))
+        donation.save()
+        return redirect(url_for('all'))
+    else:
+        donors = Donor.select()
+        return render_template('create.jinja2', donors=donors)
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 6738))
